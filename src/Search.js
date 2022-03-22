1db8c2b57a11578f1c2ee8c6c57e9ef141cce671
Daniel <daniel-mcintyre@hotmail.com>
2022-03-22 08:45:53 -0400

    added usergif search feature

--- src/Search.js
+++ src/Search.js
@@ -5,13 +5,29 @@ import axios from 'axios';
 
 function Search(props) {
 
+    // 1) initialize state to track the changing value of the user input on the search form //
+
+    const [searchValue, setSearchValue] = useState('');
+
+    // 2) Track the change event on the search form element 
+
+    const handleChange = function (event) {
+
+        // 3) take the value of the user’s search query and save it in state //
+
+        setSearchValue(event.target.value);
+
+    }
+
+    // 4) make a axios call to retrive API data to take in user search query //
+
     useEffect(() => {
 
         axios({
             url: 'https://api.giphy.com/v1/gifs/search',
             params: {
                 api_key: 'kv1PZUje6Yh0cj4AfwHkzGUyUUbW7WI7',
-                q: 'hamburgers',
+                q: searchValue,
                 limit: 20,
             }
         }).then((apiData) => {
@@ -29,12 +45,33 @@ function Search(props) {
 
     }, [])
 
-    console.log(props.searchResults);
+    const searchGifQuery = function (event) {
+
+        // 10)  Prevent the default on the form AKA tell is to prevent its default behavior (refreshing the page once the user submits the form - or selects videos to search)
+        event.preventDefault();
+
+    }
 
     return (
-        <p>  {`${props.searchResults[0]}`}</p>
-        // insert form here
-    );
+
+        <form action="" onSubmit={(event) => {
+            searchGifQuery(event, searchValue)
+        }} value={searchValue}>
+
+            <input type="text" placeholder="   Search for your Gifs here..." name="search" />
+
+            <button>Search</button>
+
+        </form>
+
+    )
+
+    // console.log(props.searchResults);
+
+    // return (
+    //     <p>  {`${props.searchResults[0]}`}</p>
+    //     // insert form here
+    // );
 }
 
 export default Search;
