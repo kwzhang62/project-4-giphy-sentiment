9fed17b0f55e02239aa08d96f4970f801de59846
Daniel <daniel-mcintyre@hotmail.com>
2022-03-23 15:27:49 -0400

    error handling for search bar continued

--- src/ErrorHandling.js
+++ src/ErrorHandling.js
@@ -1,11 +1,23 @@
 
+import { useState, useEffect } from 'react'
+
+
 function ErrorHandling(props) {
     console.log(props)
+
+    // create a piece of state for the error message
+
+    const [message, setMessage] = useState('test');
+
+    useEffect(() => {
+        if (props.source === 'userInput') {
+            setMessage(props.error)
+        }
+    }, [props.hasError])
+
     return (
-        <div>
-            {props.userInput === null ?
-                <h2>Please enter a gify search</h2>
-                : props.searchResults}
+        <div> {
+            props.hasError ? <h2> {message} </h2> : null}
         </div>
     )
 }
