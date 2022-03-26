63f5cd3a436973901134015241b6317d815a2dbc
Daniel <daniel-mcintyre@hotmail.com>
2022-03-26 19:35:20 -0400

    implemented responsive navbar with hamburger menu

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,20 +1,46 @@
 import { Link } from 'react-router-dom';
 
+import { useState } from 'react';
+
 
 function NavBar() {
+
+    const [hamNavbarOpen, setHamNavbarOpen] = useState(false)
+
+    const handleToggle = () => {
+        setHamNavbarOpen(!hamNavbarOpen)
+    }
+
     return (
-        <nav className='nav-bar-nav'>
-            <iframe src="https://giphy.com/embed/5GoVLqeAOo6PK" width="100" height="100" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
-            <h1>Giphy Sentiment</h1>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/savedGifsDisplay">Display Saved Gifs</Link>
-                </li>
-            </ul>
-        </nav>
+        <>
+            <nav className='nav-bar-nav'>
+                <iframe src="https://giphy.com/embed/5GoVLqeAOo6PK" width="100" height="100" frameBorder="0" className="giphy-embed" allowFullScreen></iframe>
+                <h1>Giphy Sentiment</h1>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/savedGifsDisplay">Display Saved Gifs</Link>
+                    </li>
+                </ul>
+            </nav>
+            <div className="wrapper">
+                <div id="hamburger-icon">
+                    <button onClick={handleToggle}><i class="fas fa-bars"></i>{hamNavbarOpen ? <div className='mobile-menu wrapper'>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/savedGifsDisplay">Display Saved Gifs</Link>
+                            </li>
+                        </ul>
+                    </div>
+                        : ""}</button>
+                </div>
+            </div>
+        </>
     )
 }
 
